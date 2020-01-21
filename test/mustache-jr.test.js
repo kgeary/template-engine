b7829a0aacf38f007bfd848fa73816638496397b
Kevin Geary <kgeary@gmail.com>
2020-01-20 20:46:29 -0600

    Added mustache-jr test

---
+++ test/mustache-jr.test.js
@@ -0,0 +1,61 @@
+const mj = require('../lib/mustache-jr');
+
+describe("mustache-jr templating", function() {
+  it("should replace data fields for templates", function() {
+    const testValue = "a {{ name }} b";
+    const testObj = {name: "test"};
+    const result = mj.update(testValue, testObj);
+    expect(result).toBe("a test b");
+  });
+  
+  it("should not replace data fields not present in the object templates", function() {
+    const testValue = "a {{ name }} b";
+    const testObj = {title: "test"};
+    const result = mj.update(testValue, testObj);
+    expect(result).toBe("a {{ name }} b");
+  });
+  
+  it("should replace mutiple occurences of fields in templates", function() {
+    const testValue = "a {{ name }} b {{ age }} c {{ name }} d {{ age }}";
+    const testObj = {name: "test", age: "21"};
+    const result = mj.update(testValue, testObj);
+    expect(result).toBe("a test b 21 c test d 21");
+  });
+
+  it("should replace over multiple lines", function() {
+    const testValue = `
+    a {{ name }} 
+    b {{ age }} 
+    c {{ name }} 
+    d {{ age }}`;
+    const testObj = {name: "test", age: "21"};
+    const result = mj.update(testValue, testObj);
+    expect(result.indexOf("{{")).toBe(-1);
+  });
+
+
+  it("should replace methods", function() {
+    const testValue = "{{ getName() }}";
+    class TestClass {
+      constructor() {
+        this.name = "test";
+        this.age = "21";
+      }
+      getName() {
+        return this.age;
+      }
+    }
+
+    const testObj = new TestClass();
+    const result = mj.update(testValue, testObj);
+    expect(result).toBe("21");
+  });
+
+  it("should not replace methods with args", function() {
+    const testValue = "{{ getName(false) }}";
+    const testObj = {name: "test", age: "21", getName: () => "Banana"};
+    const result = mj.update(testValue, testObj);
+    expect(result).toBe("{{ getName(false) }}");
+  });
+
+});
